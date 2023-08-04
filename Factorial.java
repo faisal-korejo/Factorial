1a8b1bed00e1c58c4c8bd7dfa79a4481aeadd06d
Faisal Zama Korejo <110422994+faisal-korejo@users.noreply.github.com>
2023-08-05 01:25:55 +0500

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int fact=1;
+
+        Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        int i;
+        for ( i=1; i<=input; i++){
+            fact*=i;
+
+        }
+        System.out.println("The factorial of "+input+" is:"+fact);
+    }
+}
